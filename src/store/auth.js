de7c6ea9305463cb5b0392f9e6b443f5cd31f953
Erik Bleckhorns <erik.bleckhorns@gmail.com>
2022-07-15 10:29:47 +0200

    Fixed prettier

--- src/store/auth.js
+++ src/store/auth.js
@@ -3,7 +3,8 @@ import AuthService from '../services/AuthService.js'
 //const clientid = process.env.VUE_APP_CLIENT_ID
 //const authority = process.env.VUE_APP_AUTHORITY
 const clientid = '6dcb60bd-625f-49f2-9e36-20b5c9cdc873'
-const authority = 'https://login.microsoftonline.com/f2e6e048-788b-439f-ab45-6fed3499a1f2/'
+const authority =
+  'https://login.microsoftonline.com/f2e6e048-788b-439f-ab45-6fed3499a1f2/'
 const scopes = ['profile', 'email']
 
 console.log(`clientid = ${clientid}`)
