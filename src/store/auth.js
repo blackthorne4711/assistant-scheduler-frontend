0bdd7a3c6ef307085d25a3da6e84e00340e0895c
Erik Bleckhorns <erik.bleckhorns@gmail.com>
2022-07-15 10:17:50 +0200

    Work-around for issue with env and Static web apps

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,7 +1,9 @@
 import AuthService from '../services/AuthService.js'
 
-const clientid = process.env.VUE_APP_CLIENT_ID
-const authority = process.env.VUE_APP_AUTHORITY
+//const clientid = process.env.VUE_APP_CLIENT_ID
+//const authority = process.env.VUE_APP_AUTHORITY
+const clientid = '6dcb60bd-625f-49f2-9e36-20b5c9cdc873'
+const authority = 'https://login.microsoftonline.com/f2e6e048-788b-439f-ab45-6fed3499a1f2/'
 const scopes = ['profile', 'email']
 
 console.log(`clientid = ${clientid}`)
